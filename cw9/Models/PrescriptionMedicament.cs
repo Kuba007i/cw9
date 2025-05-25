72e422689686b4680c6399b4ffbb598cff4b7a7b
s26582 <s26582@pjwstk.edu.pl>
2025-05-25 22:08:57 +0200

    :)

--- cw9/Models/PrescriptionMedicament.cs
+++ cw9/Models/PrescriptionMedicament.cs
@@ -5,7 +5,7 @@ public class PrescriptionMedicament
     public int IdMedicament { get; set; }
     public Medicament Medicament { get; set; } = null!;
 
-    public int IdPrescription { get; set; }
+    public int IdPrescription { get; set; } 
     public Prescription Prescription { get; set; } = null!;
 
     public int Dose { get; set; }
